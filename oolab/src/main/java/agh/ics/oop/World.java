e51423fb19b0326668177fb5cdff1c073bded930
michal200292 <84624327+michal200292@users.noreply.github.com>
2022-10-09 22:19:29 +0200

    Add files via upload

---
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,38 @@
+package agh.ics.oop;
+
+public class World {
+    public static void main(String[] args){
+        System.out.println("Start");
+        run(string_to_enum(args));
+        System.out.println("Stop");
+    }
+    public static void run(Direction[] moves){
+
+        for(Direction move:moves) {
+            String message = switch (move) {
+                case FORWARD -> "Zwierzak idzie do przodu";
+                case BACKWARD -> "Zwierzak idzie do tyłu";
+                case RIGHT -> "Zwierzak skręca w prawo";
+                case LEFT -> "Zwierzak skręca w lewo";
+                case UNKNOWN -> "Zwierzak stoi w miejscu";
+            };
+            System.out.println(message);
+        }
+    }
+    public static Direction[] string_to_enum(String[] args){
+        Direction[] converted = new Direction[args.length];
+        int i = 0;
+        for(String x : args){
+            Direction move = switch(x){
+                case "f" -> Direction.FORWARD;
+                case "d" -> Direction.BACKWARD;
+                case "r" -> Direction.RIGHT;
+                case "l" -> Direction.LEFT;
+                default -> Direction.UNKNOWN;
+            };
+            converted[i] = move;
+            i += 1;
+        }
+        return converted;
+    }
+}
