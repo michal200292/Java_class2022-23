6e05d974c28fa6f8dab8553e73ac5e2748f2f253
Michał Nożkiewicz <michno513@gmail.com>
2022-11-01 20:07:38 +0100

    lab3

--- oolab/src/main/java/agh/ics/oop/Animal.java
+++ oolab/src/main/java/agh/ics/oop/Animal.java
@@ -2,16 +2,22 @@ package agh.ics.oop;
 
 
 public class Animal{
-    private Vector2d position = new Vector2d(2, 2);
-    private MapDirection orientation = MapDirection.NORTH;
+    private Vector2d position;
+    private MapDirection orientation;
     private IWorldMap map;
-    public Animal(){}
+    public Animal(){
+        this.position = new Vector2d(2, 2);
+        this.orientation = MapDirection.NORTH;
+    }
     public Animal(IWorldMap map){
+        this.position = new Vector2d(2, 2);
+        this.orientation = MapDirection.NORTH;
         this.map = map;
     }
     public Animal(IWorldMap map, Vector2d initialPosition){
         this.map = map;
         this.position = initialPosition;
+        this.orientation = MapDirection.NORTH;
     }
     @Override
     public String toString(){
--- oolab/src/test/java/RectangularMapTest.java
+++ oolab/src/test/java/RectangularMapTest.java
@@ -7,11 +7,35 @@ import static org.junit.jupiter.api.Assertions.*;
 public class RectangularMapTest {
     @Test
     void test_map1() {
-        MoveDirection[] directions = new OptionParser().parse(new String[]{"f"});
-        IWorldMap map = new RectangularMap(4, 4);
-        Vector2d[] positions = { new Vector2d(2,2), new Vector2d(3,4) };
+        MoveDirection[] directions = new OptionParser().parse(new String[]{"f", "f", "f", "f", "l", "l", "r", "f", "f"});
+        IWorldMap map = new RectangularMap(6, 6);
+        Vector2d[] positions = { new Vector2d(2,2), new Vector2d(4,4), new Vector2d(6,6)};
         IEngine engine = new SimulationEngine(directions, map, positions);
         engine.run();
-        assertNotNull(map.objectAt(new Vector2d(2, 3)));
+        assertTrue(map.isOccupied(new Vector2d(2, 4)));
+        assertTrue(map.isOccupied(new Vector2d(5, 6)));
+        assertTrue(map.isOccupied(new Vector2d(3, 5)));
+    }
+
+    @Test
+    void test_map2() {
+        MoveDirection[] directions = new OptionParser().parse(new String[]{"b", "b", "b", "b", "r", "r", "f", "f", "f", "f"});
+        IWorldMap map = new RectangularMap(6, 6);
+        Vector2d[] positions = { new Vector2d(0,0), new Vector2d(1,1)};
+        IEngine engine = new SimulationEngine(directions, map, positions);
+        engine.run();
+        assertTrue(map.isOccupied(new Vector2d(1, 0)));
+        assertTrue(map.isOccupied(new Vector2d(3, 0)));
+    }
+
+    @Test
+    void test_map3() {
+        MoveDirection[] directions = new OptionParser().parse(new String[]{"f", "f", "r", "f", "f", "r", "r", "r", "f", "b", "r", "f", "f", "l", "f", "f"});
+        IWorldMap map = new RectangularMap(1, 1);
+        Vector2d[] positions = { new Vector2d(0,0), new Vector2d(1,1)};
+        IEngine engine = new SimulationEngine(directions, map, positions);
+        engine.run();
+        assertTrue(map.isOccupied(new Vector2d(0, 0)));
+        assertTrue(map.isOccupied(new Vector2d(1, 0)));
     }
 }
